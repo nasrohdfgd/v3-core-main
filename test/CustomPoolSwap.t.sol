68cfcbfd065ac6a1964581676c3984593e3eb07b
nasro <merdjinasreddine@gmail.com>
2024-12-10 16:02:02 +0200

    Initial commit

---
+++ test/CustomPoolSwap.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol"; // Import Foundry's Test library
+
+contract CustomPoolSwapTest is Test {
+    // Example of a test function
+    function testSwap() public {
+        uint x = 2 + 2;
+        assertEq(x, 4);  // This is a simple test case
+    }
+}
